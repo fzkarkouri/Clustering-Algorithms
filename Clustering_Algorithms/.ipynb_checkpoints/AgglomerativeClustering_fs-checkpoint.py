ab6215a54b1f9125444c981721cf5b571d84cfdf
Fatima Zahra KARKOURI <fz.karkouri@gmail.com>
2021-07-31 23:07:05 +0200

    Complete Linkage commit

--- Clustering_Algorithms/.ipynb_checkpoints/AgglomerativeClustering_fs-checkpoint.py
+++ Clustering_Algorithms/.ipynb_checkpoints/AgglomerativeClustering_fs-checkpoint.py
@@ -28,13 +28,17 @@ class AgglomerativeClustering_fs:
                 and the element in the row i column j is the distance between the observation i and j.
         '''
         self.similarity_matrix = squareform(pdist(X))
-        self.single(X)
+        
+        if self.linkage == 'single':
+            self.single(X)
+        elif self.linkage == 'complete':
+            self.complete(X)
         
     
     #Signle linkage 
     def single(self,X):
         '''
-        Computes the linkage matrix using a single-linkage criterion: At iteration i the most similar pair of clusters (r,s) is the pair that has the smallest distance between clusters.
+        Computes the linkage matrix using a single-linkage criterion: At iteration i the most similar pair of clusters (r,s) is the pair that has the smallest distance between clusters, the distance is the distance between the closest points.
         Input : X : A pandas dataframe containing numerical data (labels should be transformed to index) or numpy array.
         Output: A Linkage matrix a four dimensional array 
                 [Cluster i,Cluster j,Distance between Cluster i  and Cluster j, Number of Elements]
@@ -73,7 +77,48 @@ class AgglomerativeClustering_fs:
             
         self.L = np.array(self.L)
         
-         
+        
+         #Signle linkage 
+    def complete(self,X):
+        '''
+        Computes the linkage matrix using a complete-linkage criterion: At iteration i the most similar pair of clusters (r,s) is the pair that has the smallest distance between clusters. The distance is the distance between the furthest points.
+        Input : X : A pandas dataframe containing numerical data (labels should be transformed to index) or numpy array.
+        Output: A Linkage matrix a four dimensional array 
+                [Cluster i,Cluster j,Distance between Cluster i  and Cluster j, Number of Elements]
+                Cluster i and Cluster j can be singeltons.
+        '''
+        
+        #Compute the Similarity Matrix
+        np.fill_diagonal(self.similarity_matrix, np.inf)
+        self.clusters = [[i,1] for i in range(self.similarity_matrix.shape[0])]  #Clusters nbr of elements
+        m = self.similarity_matrix.shape[0]
+        nb_el=1
+        similarity_matrix_copy = self.similarity_matrix
+        
+        while(nb_el<self.similarity_matrix.shape[0]):
+
+            rs = np.unravel_index(similarity_matrix_copy.argmin(),similarity_matrix_copy.shape)
+            d_rs = similarity_matrix_copy[rs]
+            nb_el= self.clusters[rs[0]][1]+self.clusters[rs[1]][1]
+            cl = list(rs)
+            cl.append(d_rs)
+            cl.append(nb_el)
+            self.clusters.append([m,nb_el])
+            m=m+1
+
+            self.L.append(cl)
+
+            new_cluster_values = similarity_matrix_copy[[rs[0],rs[1]]].max(axis=0)
+            similarity_matrix_copy = np.concatenate((similarity_matrix_copy,[new_cluster_values]),axis=0)
+            new_cluster_values= np.append(new_cluster_values,[np.inf])
+            similarity_matrix_copy = np.concatenate((similarity_matrix_copy,np.array([new_cluster_values]).T),axis=1)
+
+            similarity_matrix_copy[:,rs[0]]=np.inf
+            similarity_matrix_copy[:,rs[1]]=np.inf
+            similarity_matrix_copy[rs[0],:]=np.inf
+            similarity_matrix_copy[rs[1],:]=np.inf 
+            
+        self.L = np.array(self.L)
         
         
 
